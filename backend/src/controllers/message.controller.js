f74e711967fedf0321d3be23b8cb81d30966cd6e
yhannis <yprudenc4@gmail.com>
2025-06-08 16:53:23 +0800

    finished backend (send and get messages) w/o realtime functionality

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,81 @@
+import cloudinary from "../lib/cloudinary.js";
+import User from "../models/user.model.js";
+
+
+
+export const getUsersForSidebar = async(req, res) => {
+
+    try {
+        
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId} }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const getMessages = async(req, res) => {
+
+    try {
+        
+        const {id : userToChatId} = req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+
+            $or: [
+                {senderId:myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId}
+            ]
+        })
+
+        res.status(200).json(messages);
+    } catch (error) {
+        
+        console.error("Error in getMessages controller: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const sendMessage = async(req,res) => {
+
+    try {
+        
+        const {text, image} = req.body
+        const { id:receiverId } = req.params
+        const senderId = req.user._id;
+
+        let imageUrl;
+         
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);;
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+
+        await newMessage.save();
+
+        //todo: realtime functionality
+
+        //201: new resource has been created.
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+
+        console.error("Error in sendMessage controller: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+        
+    }
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser"
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
     console.log("Server started at port " + port)

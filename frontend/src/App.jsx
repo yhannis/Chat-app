59c625785ce4a07393e968c00e8856477cc4e8f4
yhannis <yprudenc4@gmail.com>
2025-06-08 21:57:24 +0800

    setup react,vite, tailwind for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,26 @@
 
+import './index.css';
+import './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 
-import { useState } from 'react'
-
-
-
-const food  = [
-  {name: "potato", description: "brown thing",id: 1},
-  {name: "tomato", description: "red thing" ,id: 2},
-  {name: "cabbage", description: "green thing" ,id: 3}
-]
-
-function App() {
-
-  const [currentFood, setFood] = useState(food);
-
-  var newItem = {name: "mango", description: "yellow thing" ,id: 4};
-
-
-  function addItem(){
-
-    setFood( (currentFood) => {
-  
-
-      var newF = [...currentFood, newItem];
-
-      console.log(newF);
-      return newF;
-    });
-  }
-
- var printFood = currentFood.length == 0 ?  
- <h1> no food.</h1> 
- : currentFood.map((item) => {
-
-    return <li key={item.id}>{item.name} - {item.description}</li>
-    
- })
-
-  
-
+const App = () =>  {
 
   return (
-    <>
-      <ul>{printFood}</ul>
-      <button onClick={addItem}>Add food</button>
-    </>
+    <div>
+      
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={ <HomePage />} />
+        <Route path='/' element={ <HomePage />} />
+        <Route path='/' element={ <HomePage />} />
+        <Route path='/' element={ <HomePage />} />
+        <Route path='/' element={ <HomePage />} />
+
+      </Routes>
+    </div>
   )
 }
 
 
-
-
-export default App
+export default App;
